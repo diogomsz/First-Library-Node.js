1e994e933ca4ede93ba5b3750106c2861a43576d
Diogo Martins de Assis <diogoassis3301@gmail.com>
2022-10-12 17:00:59 -0300

    refatorando a funcao pegaArquivo com async e await

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não foi encontrado arquivo no diretório'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-
-    fs.promises
-        .readFile(caminhoDoArquivo, encoding)
-        .then(texto => console.log(chalk.green(texto)))
-        .catch(trataErro); // (erro) => trataErro(erro)
+// CODIGO ASSINCRONO COM ASYNC AWAIT
+async function pegaArquivo(caminhoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoArquivo, encoding);
+        console.log(chalk.green(texto));
+    } catch(erro) {
+        trataErro(erro);
+    }
 }
 
+// CODIGO ASSINCRONO COM THEN
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then(texto => console.log(chalk.green(texto)))
+//         .catch(trataErro); // (erro) => trataErro(erro)
+// }
+
+
+// CODIGO SEQUENCIAL
 // function pegaArquivo(caminhoDoArquivo) {
 //     const encoding = 'utf-8';
 //     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
