f8226a35eb3c8e16376fb3015b9292416b08295d
Diogo Martins de Assis <diogoassis3301@gmail.com>
2022-10-14 21:20:42 -0300

    expressao regular

--- index.js
+++ index.js
@@ -17,26 +17,6 @@ async function pegaArquivo(caminhoArquivo) {
     }
 }
 
-// CODIGO ASSINCRONO COM THEN
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-
-//     fs.promises
-//         .readFile(caminhoDoArquivo, encoding)
-//         .then(texto => console.log(chalk.green(texto)))
-//         .catch(trataErro); // (erro) => trataErro(erro)
-// }
-
-
-// CODIGO SEQUENCIAL
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//         if(erro) {
-//             trataErro(erro);
-//         }
-//         console.log(chalk.green(texto));
-//     })
-// }
-
 pegaArquivo('./arquivos/texto.md');
+
+// \[[^[\]]*?\]
